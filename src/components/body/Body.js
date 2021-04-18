f8a396307c6a1cd6fbfde3c1060b6e36d307ed8e
taewo <moondingie5@gmail.com>
2021-04-18 17:22:23 +0900

    Add console

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -8,13 +8,19 @@ function Body({ data, pageSize, page, keys, additionalCols, columns }) {
         .map((singleData, index) => {
           return columns ? (
             <tr key={index + 'rft'}>
-              {columns.map((col) => (
-                <td key={col.key}>
-                  {col.td
-                    ? col.td(singleData)
-                    : getProp(singleData, col.key.split('.')).toString()}
-                </td>
-              ))}
+              {columns.map((col) => {
+                console.log(1, col)
+                console.log(2, singleData)
+                console.log(3, col.key.split('.'))
+                console.log(4, getProp(singleData, col.key.split('.')))
+                return (
+                  <td key={col.key}>
+                    {col.td
+                      ? col.td(singleData)
+                      : getProp(singleData, col.key.split('.')).toString()}
+                  </td>
+                )
+              })}
             </tr>
           ) : (
             <tr key={index + 'rft'}>
